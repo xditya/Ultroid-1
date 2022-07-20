8ded6fc73516e769f9c6d9ef366e7448aae68a48
Lapadde <108146821+Lapadde@users.noreply.github.com>
2022-07-21 00:51:40 +0800

    Update filter.py

--- plugins/filter.py
+++ plugins/filter.py
@@ -25,59 +25,160 @@ from pyUltroid.functions.tools import create_tl_btn, format_btn, get_msg_button
 from telegraph import upload_file as uf
 from telethon.tl.types import User
 from telethon.utils import pack_bot_file_id
+from telethon.utils import get_display_name
+
+from ..core.managers import edit_or_reply
+from ..sql_helper.filter_sql import (
+    add_filter,
+    get_filters,
+    remove_all_filters,
+    remove_filter,
+)
 
 from . import events, get_string, mediainfo, udB, ultroid_bot, ultroid_cmd
 from ._inline import something
 
 
-@ultroid_cmd(pattern="addfilter( (.*)|$)")
-async def af(e):
-    wrd = (e.pattern_match.group(1).strip()).lower()
-    wt = await e.get_reply_message()
-    chat = e.chat_id
-    if not (wt and wrd):
-        return await e.eor(get_string("flr_1"))
-    btn = format_btn(wt.buttons) if wt.buttons else None
-    if wt and wt.media:
-        wut = mediainfo(wt.media)
-        if wut.startswith(("pic", "gif")):
-            dl = await wt.download_media()
-            variable = uf(dl)
-            m = "https://telegra.ph" + variable[0]
-        elif wut == "video":
-            if wt.media.document.size > 8 * 1000 * 1000:
-                return await e.eor(get_string("com_4"), time=5)
-            dl = await wt.download_media()
-            variable = uf(dl)
-            os.remove(dl)
-            m = "https://telegra.ph" + variable[0]
+@ultroid_cmd(incoming=True)
+async def filter_incoming_handler(event):  # sourcery no-metrics
+    if event.sender_id == event.client.uid:
+        return
+    name = event.raw_text
+    filters = get_filters(event.chat_id)
+    if not filters:
+        return
+    a_user = await event.get_sender()
+    chat = await event.get_chat()
+    me = await event.client.get_me()
+    title = get_display_name(await event.get_chat()) or "this chat"
+    participants = await event.client.get_participants(chat)
+    count = len(participants)
+    mention = f"[{a_user.first_name}](tg://user?id={a_user.id})"
+    my_mention = f"[{me.first_name}](tg://user?id={me.id})"
+    first = a_user.first_name
+    last = a_user.last_name
+    fullname = f"{first} {last}" if last else first
+    username = f"@{a_user.username}" if a_user.username else mention
+    userid = a_user.id
+    my_first = me.first_name
+    my_last = me.last_name
+    my_fullname = f"{my_first} {my_last}" if my_last else my_first
+    my_username = f"@{me.username}" if me.username else my_mention
+    for trigger in filters:
+        pattern = r"( |^|[^\w])" + re.escape(trigger.keyword) + r"( |$|[^\w])"
+        if re.search(pattern, name, flags=re.IGNORECASE):
+            file_media = None
+            filter_msg = None
+            if trigger.f_mesg_id:
+                msg_o = await event.client.get_messages(
+                    entity=BOTLOG_CHATID, ids=int(trigger.f_mesg_id)
+                )
+                file_media = msg_o.media
+                filter_msg = msg_o.message
+                link_preview = True
+            elif trigger.reply:
+                filter_msg = trigger.reply
+                link_preview = False
+            await event.reply(
+                filter_msg.format(
+                    mention=mention,
+                    title=title,
+                    count=count,
+                    first=first,
+                    last=last,
+                    fullname=fullname,
+                    username=username,
+                    userid=userid,
+                    my_first=my_first,
+                    my_last=my_last,
+                    my_fullname=my_fullname,
+                    my_username=my_username,
+                    my_mention=my_mention,
+                ),
+                file=file_media,
+                link_preview=link_preview,
+            )
+
+@ultroid_cmd(
+    pattern="filter (.*)",
+    command=("filter", plugin_category),
+    info={
+        "header": "To save filter for the given keyword.",
+        "description": "If any user sends that filter then your bot will reply.",
+        "option": {
+            "{mention}": "To mention the user",
+            "{title}": "To get chat name in message",
+            "{count}": "To get group members",
+            "{first}": "To use user first name",
+            "{last}": "To use user last name",
+            "{fullname}": "To use user full name",
+            "{userid}": "To use userid",
+            "{username}": "To use user username",
+            "{my_first}": "To use my first name",
+            "{my_fullname}": "To use my full name",
+            "{my_last}": "To use my last name",
+            "{my_mention}": "To mention myself",
+            "{my_username}": "To use my username.",
+        },
+        "usage": "{tr}filter <keyword>",
+    },
+)
+async def add_new_filter(event):
+    "To save the filter"
+    keyword = event.pattern_match.group(1)
+    string = event.text.partition(keyword)[2]
+    msg = await event.get_reply_message()
+    msg_id = None
+    if msg and msg.media and not string:
+        if BOTLOG:
+            await event.client.send_message(
+                BOTLOG_CHATID,
+                f"#FILTER\
+            \nCHAT ID: {event.chat_id}\
+            \nTRIGGER: {keyword}\
+            \n\nThe following message is saved as the filter's reply data for the chat, please do NOT delete it !!",
+            )
+            msg_o = await event.client.forward_messages(
+                entity=BOTLOG_CHATID,
+                messages=msg,
+                from_peer=event.chat_id,
+                silent=True,
+            )
+            msg_id = msg_o.id
         else:
-            m = pack_bot_file_id(wt.media)
-        if wt.text:
-            txt = wt.text
-            if not btn:
-                txt, btn = get_msg_button(wt.text)
-            add_filter(chat, wrd, txt, m, btn)
-        else:
-            add_filter(chat, wrd, None, m, btn)
+            await edit_or_reply(
+                event,
+                "__Saving media as reply to the filter requires the__ `PRIVATE_GROUP_BOT_API_ID` __to be set.__",
+            )
+            return
+    elif msg and msg.text and not string:
+        string = msg.text
+    elif not string:
+        return await edit_or_reply(event, "__What should i do ?__")
+    success = "`Filter` **{}** `{} successfully`"
+    if add_filter(str(event.chat_id), keyword, string, msg_id) is True:
+        return await edit_or_reply(event, success.format(keyword, "added"))
+    remove_filter(str(event.chat_id), keyword)
+    if add_filter(str(event.chat_id), keyword, string, msg_id) is True:
+        return await edit_or_reply(event, success.format(keyword, "Updated"))
+    await edit_or_reply(event, f"Error while setting filter for {keyword}")
+
+
+@ultroid_cmd((
+    pattern="stop ([\s\S]*)",
+    command=("stop", plugin_category),
+    info={
+        "header": "To delete that filter . so if user send that keyword bot will not reply",
+        "usage": "{tr}stop <keyword>",
+    },
+)
+async def remove_a_filter(event):
+    "Stops the specified keyword."
+    filt = event.pattern_match.group(1)
+    if not remove_filter(event.chat_id, filt):
+        await event.edit("Filter` {} `doesn't exist.".format(filt))
     else:
-        txt = wt.text
-        if not btn:
-            txt, btn = get_msg_button(wt.text)
-        add_filter(chat, wrd, txt, None, btn)
-    await e.eor(get_string("flr_4").format(wrd))
-    ultroid_bot.add_handler(filter_func, events.NewMessage())
-
-
-@ultroid_cmd(pattern="remfilter( (.*)|$)")
-async def rf(e):
-    wrd = (e.pattern_match.group(1).strip()).lower()
-    chat = e.chat_id
-    if not wrd:
-        return await e.eor(get_string("flr_3"))
-    rem_filter(int(chat), wrd)
-    await e.eor(get_string("flr_5").format(wrd))
-
+        await event.edit("Filter `{} `was deleted successfully".format(filt))
 
 @ultroid_cmd(pattern="listfilter$")
 async def lsnote(e):
